64bb24f2eaf3828f37b56104790f0c48474177d9
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-28 19:27:54 +0100

    feat: Add Event schema to tim_events_api module
    This commit adds the Event schema to the tim_events_api module. The schema includes fields for name, description, location, start_time, end_time, organizer_id, and speakers. It extends the EventBase schema and enables the validation and serialization of event data. This addition enhances the functionality of the tim_events_api module by providing a structured representation of events.

---
+++ tim_events_api/schemas/schema_events.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from .schema_speakers import Speaker
+from datetime import datetime
+
+class EventBase(BaseModel):
+        name: str
+        description: str
+        location: str
+        start_time:datetime
+        end_time: datetime
+
+
+
+class EventCreate(EventBase):
+        pass
+
+class EventUpdate(EventBase):
+        pass
+
+class Event(EventBase):
+        organizer_id: int
+        speakers: list[Speaker] = []
+        
+        class Config():
+                orm_mode = True
