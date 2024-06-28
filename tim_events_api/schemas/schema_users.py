44ed2c7c75c5757b298889587caa3d2a12ddb60d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-28 19:28:52 +0100

    feat: Add User schema to tim_events_api module
    This commit adds the User schema to the tim_events_api module. The schema includes fields for email, first_name, last_name, and username. It extends the UserBase schema and enables the validation and serialization of user data. This addition enhances the functionality of the tim_events_api module by providing a structured representation of users.

---
+++ tim_events_api/schemas/schema_users.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import Annotated
+from .schema_events import Event
+
+
+
+class UserBase(BaseModel):
+        email: EmailStr
+        first_name: str
+        last_name: str
+        username: str
+
+class UserCreate(UserBase):
+        password: str
+
+class UserUpdate(UserBase):
+        pass
+
+class User(UserBase):
+        id: int
+        is_active: bool
+        is_organizer: bool
+        is_admin: bool
+        events: list[Event] = []
+
+        class Config:
+                orm_mode = True
