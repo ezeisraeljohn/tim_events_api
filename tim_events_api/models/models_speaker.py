3d8c0a017efb7003ac950b017375cd90e2eb191a
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-28 19:25:04 +0100

    feat: Add Speaker model
    This commit adds the Speaker model to the tim_events_api module. The model includes columns for first_name, last_name, bio, profile_picture, contact_info, and event_id. It also establishes a relationship with the Event model. This addition enables CRUD operations for speakers in the module.

---
+++ tim_events_api/models/models_speaker.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, DateTime, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from ..database import Base
+from .models_base import BaseModel
+
+
+class Speaker(BaseModel, Base):
+      __tablename__ = 'speakers'
+
+      first_name = Column(String(60))
+      last_name = Column(String(60))
+      bio = Column(Text)
+      profile_picture = Column(String(255))
+      contact_info = Column(String(255))
+      event_id = Column(Integer, ForeignKey('events.id'))
+
+      event = relationship("Event", back_populates="speakers")
