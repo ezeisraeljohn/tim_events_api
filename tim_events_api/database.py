49b6f011a0973d6ea1ef514fda68751c6310f7fa
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-30 01:34:38 +0100

    Made chanes to how the engine is created

--- tim_events_api/database.py
+++ tim_events_api/database.py
@@ -2,10 +2,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
-engine = create_engine(url=SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+CA_CERT_PATH=os.getenv("CA_CERT_PATH")
+engine = create_engine(url=SQLALCHEMY_DATABASE_URL,
+        connect_args={
+             "ssl": {
+                 "sslmode": "REQUIRED",
+                 "ca": CA_CERT_PATH,  
+             },
+        }
+        )
 
 Base = declarative_base()
 
