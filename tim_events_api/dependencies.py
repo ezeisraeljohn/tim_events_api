75c192b7c1cd462dffc610f2058ec9ef71d737b4
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-30 01:44:15 +0100

    refactor: Update dependencies.py
    Refactor the dependencies.py file to remove unnecessary imports and update the SECRET_KEY retrieval method. This change improves the efficiency and security of the code by using the getenv function instead of get to retrieve the SECRET_KEY environment variable.

--- tim_events_api/dependencies.py
+++ tim_events_api/dependencies.py
@@ -1,10 +1,8 @@
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordBearer
 from .database import SessionLocal
-import jwt
 import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
