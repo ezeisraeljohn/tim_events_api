2b8910473c8d3d3f29ead1f111e36c57e8816b15
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-01 03:29:50 +0100

    refactor: Update CRUD operations for speakers

--- tim_events_api/crud/users.py
+++ tim_events_api/crud/users.py
@@ -1,4 +1,3 @@
-
 """ This module contains the CRUD operations for the users table.
 The functions are:
 - get_user: Get a single user by its id.
@@ -18,6 +17,7 @@ each function interacts with the database session object to perform the CRUD ope
 For more information on how to perform CRUD operations,
 see: https://fastapi.tiangolo.com/tutorial/sql-databases/#create-the-crud-utilities
 """
+
 from sqlalchemy.orm import Session
 from ..models import models_user
 from ..schemas import schema_users, schema_token
@@ -28,202 +28,212 @@ from fastapi import Depends, HTTPException, status
 from ..database import SessionLocal
 import jwt
 from jwt.exceptions import InvalidTokenError
+from email_validator import validate_email, EmailNotValidError
 
 
 pwd_content = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def get_user(db: Session, user_id: int):
-        """Get a single user by its id.
-        Args:
-        db (Session): The database session.
-        user_id (int): The id of the user.
-        
-        Returns:
-        User: The user object.
-        """
-        return db.query(models_user.User).filter(models_user.User.id == user_id).first()
+    """Get a single user by its id.
+    Args:
+    db (Session): The database session.
+    user_id (int): The id of the user.
+
+    Returns:
+    User: The user object.
+    """
+    return db.query(models_user.User).filter(models_user.User.id == user_id).first()
 
 
 def get_user_by_username(db: Session, username: str):
-        """Get a single user by its username.
-        Args:
-        db (Session): The database session.
-        username (str): The username of the user.
-        Returns:
-        User: The user object.
-        """
-        return db.query(models_user.User).filter(models_user.User.username == username).first()
+    """Get a single user by its username.
+    Args:
+    db (Session): The database session.
+    username (str): The username of the user.
+    Returns:
+    User: The user object.
+    """
+    return (
+        db.query(models_user.User).filter(models_user.User.username == username).first()
+    )
 
 
 def get_user_by_email(db: Session, email: str):
-        """Get a single user by its email.
-        Args:
-        db (Session): The database session.
-        email (str): The email of the user.
-        Returns:
-        User: The user object.
-        """
-        return db.query(models_user.User).filter(models_user.User.email == email).first()
+    """Get a single user by its email.
+    Args:
+    db (Session): The database session.
+    email (str): The email of the user.
+    Returns:
+    User: The user object.
+    """
+    return db.query(models_user.User).filter(models_user.User.email == email).first()
+
+
+def is_valid_email(email):
+    validate_email(email=email)
+    return True
 
 
 def hash_password(password: str):
-        """Hash a password.
-        Args:
-        password (str): The password to hash.
-        Returns:
-        str: The hashed password.
-        """
-        return pwd_content.hash(password)
+    """Hash a password.
+    Args:
+    password (str): The password to hash.
+    Returns:
+    str: The hashed password.
+    """
+    return pwd_content.hash(password)
 
 
 def verify_password(plain_password, hashed_password):
-        """Verify a password.
-        Args:
-        plain_password (str): The plain password.
-        hashed_password (str): The hashed password.
+    """Verify a password.
+    Args:
+    plain_password (str): The plain password.
+    hashed_password (str): The hashed password.
 
-        Returns:
-        bool: True if the password is valid, False otherwise.
-        """
-        return pwd_content.verify(plain_password, hashed_password)
+    Returns:
+    bool: True if the password is valid, False otherwise.
+    """
+    return pwd_content.verify(plain_password, hashed_password)
 
 
 def authenticate_user(db: get_db, username: str, password: str):
-        """Authenticate a user.
-        Args:
-        db (Session): The database session.
-        username (str): The username of the user.
-        password (str): The password of the user.
-        
-        Returns:
-        User: The user object.
-        """
-        user = get_user_by_username(db=db, username=username)
-        if not user:
-               return False
-        the_password = verify_password(password, user.hashed_password)
-        if not the_password:
-               return False
-        return user
+    """Authenticate a user.
+    Args:
+    db (Session): The database session.
+    username (str): The username of the user.
+    password (str): The password of the user.
+
+    Returns:
+    User: The user object.
+    """
+    user = get_user_by_username(db=db, username=username)
+    if not user:
+        return False
+    the_password = verify_password(password, user.hashed_password)
+    if not the_password:
+        return False
+    return user
 
 
 def create_access_token(data: dict, expire_timdelta: timedelta | None = None):
-        """Create an access token.
-        Args:
-        data (dict): The data to encode in the token.
-        expire_timdelta (timedelta): The expiration time of the token.
-        
-        Returns:
-        str: The encoded token.
-        """
-        to_encode = data.copy()
+    """Create an access token.
+    Args:
+    data (dict): The data to encode in the token.
+    expire_timdelta (timedelta): The expiration time of the token.
 
-        if expire_timdelta:
-                expire = datetime.now(timezone.utc) + expire_timdelta
-        else:
-                expire = datetime.now(timedelta.utc) + timedelta(minutes=15)
+    Returns:
+    str: The encoded token.
+    """
+    to_encode = data.copy()
 
-        to_encode['exp'] = expire
-        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
-        return encoded_jwt
+    if expire_timdelta:
+        expire = datetime.now(timezone.utc) + expire_timdelta
+    else:
+        expire = datetime.now(timedelta.utc) + timedelta(minutes=15)
+
+    to_encode["exp"] = expire
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+    return encoded_jwt
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
-        """Get the current user.
-        Args:
-        token (str): The access token.
-        
-        Returns:
-        User: The user object.
-        """
-        credential_error = HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate credentials",
-                headers={"WWW-Authenticate": "Bearer"},
-                                )
-        try:
-                payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-                username: str = payload.get('sub')
-                if username is None:
-                        raise credential_error
-                token_data = schema_token.TokenData(username=username)
+    """Get the current user.
+    Args:
+    token (str): The access token.
 
-        except InvalidTokenError:
-                raise credential_error
+    Returns:
+    User: The user object.
+    """
+    credential_error = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credential_error
+        token_data = schema_token.TokenData(username=username)
 
-        user = get_user_by_username(SessionLocal(), username=token_data.username)
+    except InvalidTokenError:
+        raise credential_error
 
-        if user is None:
-                return credential_error
-        return user
+    user = get_user_by_username(SessionLocal(), username=token_data.username)
+
+    if user is None:
+        return credential_error
+    return user
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-        """Get all users.
-        Args:
-        db (Session): The database session.
-        skip (int): The number of users to skip.
-        limit (int): The number of users to return.
-        
-        Returns:
-        List[User]: A list of user objects.
-        """
-        return db.query(models_user.User).offset(skip).limit(limit).all()
+    """Get all users.
+    Args:
+    db (Session): The database session.
+    skip (int): The number of users to skip.
+    limit (int): The number of users to return.
+
+    Returns:
+    List[User]: A list of user objects.
+    """
+    return db.query(models_user.User).offset(skip).limit(limit).all()
 
 
 def add_user(db: Session, user: schema_users.UserCreate):
-        """Add a user.
-        Args:
-        db (Session): The database session.
-        user (UserCreate): The user data.
+    """Add a user.
+    Args:
+    db (Session): The database session.
+    user (UserCreate): The user data.
 
-        Returns:
-        User: The user object.
-        """
-        hashed_password = hash_password(user.password)
-        db_user = models_user.User(
-                first_name=user.first_name,
-                last_name=user.last_name,
-                hashed_password=hashed_password,
-                email=user.email,
-                username=user.username)
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
-        return db_user
+    Returns:
+    User: The user object.
+    """
+    hashed_password = hash_password(user.password)
+    valid_email = is_valid_email(user.email)
+    db_user = models_user.User(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        hashed_password=hashed_password,
+        email=(user.email),
+        username=user.username,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
-def edit_user(db:Session, user_id: int, user:schema_users.UserUpdate):
-        """Edit a user.
-        Args:
-        db (Session): The database session.
-        user_id (int): The id of the user.
-        user (UserUpdate): The user data.
+def edit_user(db: Session, user_id: int, user: schema_users.UserUpdate):
+    """Edit a user.
+    Args:
+    db (Session): The database session.
+    user_id (int): The id of the user.
+    user (UserUpdate): The user data.
 
-        Returns:
-        User: The user object.
-        """
-        db_user = db.query(models_user.User).filter(models_user.User.id == user_id).first()
-        user_dict = user.dict()
+    Returns:
+    User: The user object.
+    """
+    db_user = db.query(models_user.User).filter(models_user.User.id == user_id).first()
+    user_dict = user.dict()
 
-        for key, value in user_dict.items():
-                setattr(db_user, key, value)
-        db.commit()
-        db.refresh(db_user)
-        return db_user
+    for key, value in user_dict.items():
+        setattr(db_user, key, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
 def remove_user(db: Session, user_id: int):
-        """Remove a user.
-        Args:
-        db (Session): The database session.
-        user_id (int): The id of the user.
+    """Remove a user.
+    Args:
+    db (Session): The database session.
+    user_id (int): The id of the user.
 
-        Returns:
-        dict: An empty dictionary.
-        """
-        db_user = db.query(models_user.User).filter(models_user.User.id == user_id).first()
-        db.delete(db_user)
-        db.commit()
-        return {}
-
+    Returns:
+    dict: An empty dictionary.
+    """
+    db_user = db.query(models_user.User).filter(models_user.User.id == user_id).first()
+    db.delete(db_user)
+    db.commit()
+    return {}
