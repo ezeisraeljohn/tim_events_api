ef937abaa3a5560876c839ebfd8e6984bbd74127
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-01 03:32:28 +0100

    Refactor User model for improved readability and consistency
    Refactor the User model in models_user.py to improve code readability and maintainability. This change includes reformatting the code and ensuring consistency with the established conventions for the User model in the project.

--- tim_events_api/models/models_user.py
+++ tim_events_api/models/models_user.py
@@ -3,21 +3,20 @@ from ..database import Base
 from .models_base import BaseModel
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
-        __tablename__ = 'users'
+    __tablename__ = "users"
 
-        first_name = Column(String(60))
-        username = Column(String(70))
-        last_name = Column(String(60))
-        email = Column(String(60))
-        hashed_password = Column(String(60))
-        is_active = Column(Boolean, default=True)
-        is_organizer = Column(Boolean, default=True)
-        is_admin = Column(Boolean, default=False)
+    first_name = Column(String(60))
+    username = Column(String(70))
+    last_name = Column(String(60))
+    email = Column(String(60))
+    hashed_password = Column(String(60))
+    is_active = Column(Boolean, default=True)
+    is_organizer = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
 
-        events = relationship("Event", back_populates="user")
-       
+    events = relationship("Event", back_populates="user")
 
-        def __str__(self):
-                return(f"{self.first_name} {self.last_name}")
-        
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
