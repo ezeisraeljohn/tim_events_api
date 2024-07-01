2abb2e6d7748ecc1e9f94b472ba4d6e3a69847e9
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-01 03:37:18 +0100

    Refactor User model for improved readability and consistency

--- tim_events_api/schemas/schema_users.py
+++ tim_events_api/schemas/schema_users.py
@@ -3,25 +3,27 @@ from typing import Annotated
 from .schema_events import Event
 
 
-
 class UserBase(BaseModel):
-        email: EmailStr
-        first_name: str
-        last_name: str
-        username: str
+    email: EmailStr
+    first_name: str
+    last_name: str
+    username: str
+
 
 class UserCreate(UserBase):
-        password: str
+    password: str
+
 
 class UserUpdate(UserBase):
-        pass
+    pass
+
 
 class User(UserBase):
-        id: int
-        is_active: bool
-        is_organizer: bool
-        is_admin: bool
-        events: list[Event] = []
+    id: int
+    is_active: bool
+    is_organizer: bool
+    is_admin: bool
+    events: list[Event] = []
 
-        class Config:
-                orm_mode = True
+    class Config:
+        orm_mode = True
