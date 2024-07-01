8c99035fd956997829a45b17b650fde402d25eaf
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-01 03:38:24 +0100

    Refactor Event schema for improved readability and consistency
    Refactor the Event schema in schema_events.py to improve code readability and maintainability. This change includes reformatting the code and ensuring consistency with the established conventions for the Event schema in the project.

--- tim_events_api/schemas/schema_events.py
+++ tim_events_api/schemas/schema_events.py
@@ -2,25 +2,27 @@ from pydantic import BaseModel
 from .schema_speakers import Speaker
 from datetime import datetime
 
-class EventBase(BaseModel):
-        name: str
-        description: str
-        location: str
-        start_time:datetime
-        end_time: datetime
 
+class EventBase(BaseModel):
+    name: str
+    description: str
+    location: str
+    start_time: datetime
+    end_time: datetime
 
 
 class EventCreate(EventBase):
-        pass
+    pass
+
 
 class EventUpdate(EventBase):
-        pass
+    pass
+
 
 class Event(EventBase):
-        id: int
-        organizer_id: int
-        speakers: list[Speaker] = []
-        
-        class Config():
-                orm_mode = True
+    id: int
+    organizer_id: int
+    speakers: list[Speaker] = []
+
+    class Config:
+        orm_mode = True
